c16fe99f288e4f9d6286fd6f4bebe64fdf4f49e9
kayhon <kayhon@gmail.com>
2018-03-14 20:30:41 -0700

    pseudo4all_done

---
+++ assets/javascript/game.js
@@ -0,0 +1,44 @@
+//<!--//
+//
+//<!--links-->
+     //
+  // <!--//
+   //<!--//alert explicit material
+
+ //Need function to starts the game, and happens every time after win/loose
+ //(gameStart) {function
+
+    //All character list 
+
+ //function that controls the user's guesses inside of the game   
+
+//start 
+//win
+//loose 
+
+//how many guess 
+//how many loss 
+//how many win 
+//
+//
+ //for loop if/else for the blank tiles (_) for the Word
+
+//code for showing the blank letter to guess
+
+//code for on click or keyup
+
+//code for not being able to press same key twice and valid buttons
+
+//code for multiple letters when chosen
+
+
+//code for win , wrong choice, double choice, loss
+
+//code for each win for each characters picture:
+//
+//character list//--> //-->
+//-->
+//-->
+//
+
+//-->
